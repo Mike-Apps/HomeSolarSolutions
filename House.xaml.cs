846f5f89272a532da63526c0ab9a3c9e4acb4901
Michael Ott <mikeottmail@gmail.com>
2022-05-07 15:25:54 -0400

    ..

---
+++ House.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HomeSolarSolutions
+{
+    /// <summary>
+    /// Interaction logic for House.xaml
+    /// </summary>
+    /// 
+    
+    public partial class House : Window
+    {
+        public readonly MainWindow _mainWindow;
+        public House(MainWindow mainwin)
+        {
+            _mainWindow = mainwin;
+            InitializeComponent();
+           
+        }
+    }
+}
